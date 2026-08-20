4113185ba88bfbbb79251a7a84f7061c65371277
Vishwanath Rathod <nvishwanathvishwanath55@gmail.com>
2026-08-20 18:33:53 +0530

    Time: 7 ms (5.38%) | Memory: 23.8 MB (83.63%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -4,7 +4,6 @@ public:
         int n  = nums.size();
         vector<int> arr1;
         vector<int> arr2;
-        vector<int> res;
         arr1.push_back(nums[0]);
         arr2.push_back(nums[1]);
         for(int i = 2; i<n; i++){
@@ -16,13 +15,9 @@ public:
                 arr2.push_back(nums[i]);
             }
         }
-        for(int i = 0;i<arr1.size();i++){
-            res.push_back(arr1[i]);
+        for(int x : arr2){
+            arr1.push_back(x);
         }
-        for(int i = 0;i<arr2.size();i++){
-            res.push_back(arr2[i]);
-        }
-        return res;
-        
+        return arr1;   
     }
 };
